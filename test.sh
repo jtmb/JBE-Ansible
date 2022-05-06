ac1f0dc288aa5c4419cf85f6004d34b983f0e4c4
jtmb <james.branco@gmail.com>
2022-05-06 20:09:41 +0000

    yes

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-http --form POST 192.168.0.38:8090/api/endpoints \
-    "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJhbnNpYmxlIiwicm9sZSI6MSwic2NvcGUiOiJkZWZhdWx0IiwiZXhwIjoxNjUxNzQxNjE5fQ.weXCZX2k2fG5my_XWm6VzS_-t-4Er5kUVoKOibGaSOM" \
-    Name="test-remote" URL="http://192.168.0.37:9091" EndpointCreationType=1
+  http --form POST http://192.168.0.38:8090/api/endpoints \
+    "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJhbnNpYmxlIiwicm9sZSI6MSwic2NvcGUiOiJkZWZhdWx0IiwiZXhwIjoxNjUxODk2NDUzfQ.14Tq9neJHThmzpKx0-LE5ulOL6sLmLN2MMksfOYfjEo" \
+    Name="test-remote" URL="tcp://192.168.0.37:9091" EndpointCreationType=1
